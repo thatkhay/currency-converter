6cac6cba9354054f97606ade4cc12c74d895ddca
thatkhay <johnkingsleyegeonu@gmail.com>
2023-07-14 21:37:02 +0100

    first commit

---
+++ currency/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import CurrencyRow from './componets/CurrencyRow';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>convert</h1>
+     <CurrencyRow/>
+     <div className=' height: 100vh; '>=</div>
+     <CurrencyRow/>
+    </div>
+  );
+}
+
+export default App;
---
+++ currency/src/componets/CurrencyRow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CurrencyRow() {
+  return (
+    <div>
+        <input type="number" className='input' />
+        <select >
+            <option value="hi">hi</option>
+        </select>
+    </div>
+  )
+}
+
+export default CurrencyRow
