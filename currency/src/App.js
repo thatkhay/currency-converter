428193c748cc055f392eb8f364aa7c35ce7c8b24
thatkhay <johnkingsleyegeonu@gmail.com>
2023-07-15 13:10:39 +0100

    axios

--- currency/src/App.js
+++ currency/src/App.js
@@ -1,13 +1,30 @@
 
+import { useEffect } from 'react';
 import './App.css';
 import CurrencyRow from './componets/CurrencyRow';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest'
+const accessKey = 'c515f1cbe6895005fa9590ab97401235'
+
 
 function App() {
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?access_key=${accessKey}`)
+      .then(response => {
+       
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
   return (
     <div className="App">
       <h1>convert</h1>
      <CurrencyRow/>
-     <div className=' height: 100vh; '>=</div>
+     <div className='equals'>=</div>
      <CurrencyRow/>
     </div>
   );
